9e8497cd9ec07a0d5113ec6ec802cb579efb0475
saber khazaei <saber.khazaei10@gmail.com>
2025-08-11 02:25:49 +0400

    Show cart count and back button on product detail

--- public/product.js
+++ public/product.js
@@ -2,6 +2,7 @@ const { useEffect, useState } = React;
 
 function ProductDetail() {
   const [product, setProduct] = useState(null);
+  const [cartCount, setCartCount] = useState(0);
   const token = localStorage.getItem('token');
   const params = new URLSearchParams(window.location.search);
   const id = params.get('id');
@@ -16,6 +17,12 @@ function ProductDetail() {
     })
       .then(res => res.json())
       .then(setProduct);
+
+    fetch('/api/cart', {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then(res => res.json())
+      .then(cart => setCartCount(cart.length));
   }, [id, token]);
 
   const addToCart = () => {
@@ -26,7 +33,10 @@ function ProductDetail() {
         Authorization: `Bearer ${token}`
       },
       body: JSON.stringify({ productId: id })
-    }).then(() => alert('به سبد خرید اضافه شد'));
+    }).then(() => {
+      alert('به سبد خرید اضافه شد');
+      setCartCount(c => c + 1);
+    });
   };
 
   if (!product) return <div>در حال بارگذاری...</div>;
@@ -37,7 +47,13 @@ function ProductDetail() {
       <img src={product.imageUrl} alt={product.name} />
       <p>{product.description}</p>
       <p>{product.price} تومان</p>
-      <button onClick={addToCart}>افزودن به سبد خرید</button>
+      <div style={{ marginTop: '10px' }}>
+        <button onClick={addToCart}>افزودن به سبد خرید</button>
+        <span style={{ marginRight: '10px' }}>({cartCount})</span>
+      </div>
+      <button style={{ marginTop: '10px' }} onClick={() => (window.location.href = '/products.html')}>
+        بازگشت
+      </button>
     </div>
   );
 }
